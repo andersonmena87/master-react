c9e43cc3c77f9eee0bc0724bd2096b1d876f6323
Anderson Mena Serna <andersonm@itcol.com>
2023-01-06 09:44:08 -0500

    Creación proyecto 11-layout-effect

---
+++ 11-layout-effect/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { EjemploComponent } from './components/EjemploComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <EjemploComponent />
+        <hr />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 11-layout-effect/src/components/EjemploComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const EjemploComponent = () => {
+  return (
+    <div>
+        <h1>Ejemplo useEffect y useLayoutEffect</h1>
+    </div>
+  )
+}
